00a6e34fa3b77c8af916c2ba67b18bcc2df5e6e2
twinklepatel66 <twinklepatel6666@gmail.com>
2026-05-20 13:09:16 +0530

    restored ProtectedRoute

--- client/src/components/Protectedroute.jsx
+++ client/src/components/Protectedroute.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({ children }) {
+const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem("token");
 
   if (!token) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/login" replace />;
   }
 
   return children;
-}
+};
+
+export default ProtectedRoute;
